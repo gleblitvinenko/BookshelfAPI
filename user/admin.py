2881be9c000f367404a440be72829beb4e6477cd
Glieb <gleblitvinenko3007@gmail.com>
2023-08-03 20:28:50 +0300

    USER: fixed admin.py & made migrations

--- user/admin.py
+++ user/admin.py
@@ -6,30 +6,35 @@ from .models import User
 
 class UserAdmin(BaseUserAdmin):
     fieldsets = (
-        (None, {'fields': ('email', 'password', 'name', 'last_login')}),
-        ('Permissions', {'fields': (
-            'is_active',
-            'is_staff',
-            'is_superuser',
-            'groups',
-            'user_permissions',
-        )}),
-    )
-    add_fieldsets = (
         (
             None,
+            {"fields": ("email", "password", "first_name", "last_name", "last_login")},
+        ),
+        (
+            "Permissions",
             {
-                'classes': ('wide',),
-                'fields': ('email', 'password1', 'password2')
-            }
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
         ),
     )
+    add_fieldsets = (
+        (None, {"classes": ("wide",), "fields": ("email", "password1", "password2")}),
+    )
 
-    list_display = ('email', 'name', 'is_staff', 'last_login')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-    search_fields = ('email',)
-    ordering = ('email',)
-    filter_horizontal = ('groups', 'user_permissions',)
+    list_display = ("email", "first_name", "last_name", "is_staff", "last_login")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("email",)
+    ordering = ("email",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
 
 
 admin.site.register(User, UserAdmin)
