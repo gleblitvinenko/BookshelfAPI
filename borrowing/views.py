aceac9fdc86750362ee9480209826e15ab036a74
Glieb <gleblitvinenko3007@gmail.com>
2023-08-04 15:30:19 +0300

    BORROWING: implemented BorrowingListView & BorrowingDetailView

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from borrowing.models import Borrowing
+from borrowing.serializers import BorrowingSerializer, BorrowingDetailSerializer
+
+
+class BorrowingListView(generics.ListAPIView):
+
+    queryset = Borrowing.objects.select_related("borrower", "book")
+    serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated, )
+
+    def get_object(self):
+        return self.request.user
+
+
+class BorrowingDetailView(generics.RetrieveAPIView):
+
+    queryset = Borrowing.objects.select_related("borrower", "book")
+    serializer_class = BorrowingDetailSerializer
+    permission_classes = (IsAuthenticated, )
+
+    def get_object(self):
+        return self.request.user
