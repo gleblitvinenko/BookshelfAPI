c9d5dd4362e04b4993d4dd1ee100861d4f463b73
Glieb <gleblitvinenko3007@gmail.com>
2023-08-05 18:29:54 +0300

    BORROWING: implemented inventory decreasing

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,6 +1,9 @@
+from django.db import transaction
 from rest_framework import generics
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 
+from book.models import Book
 from borrowing.models import Borrowing
 from borrowing.serializers import BorrowingSerializer, BorrowingDetailSerializer, CreateBorrowingSerializer
 
@@ -19,7 +22,25 @@ class BorrowingListView(generics.ListCreateAPIView):
             return CreateBorrowingSerializer
         return BorrowingSerializer
 
+    @transaction.atomic
     def perform_create(self, serializer):
+        request_data = serializer.validated_data
+        book_id = serializer.validated_data.get("book")
+        print(book_id)
+
+        try:
+            book = Book.objects.get(pk=book_id.id)
+            print(book)
+        except Book.DoesNotExist:
+            raise ValidationError("The selected book does not exist.")
+
+        book_inventory = book.inventory
+        if book_inventory > 0:
+            book.inventory -= 1
+            book.save()
+        else:
+            raise ValidationError("No such books left")
+
         serializer.save(borrower=self.request.user)
 
 
@@ -32,14 +53,3 @@ class BorrowingDetailView(generics.RetrieveAPIView):
     def get_queryset(self):
         user = self.request.user
         return Borrowing.objects.filter(borrower=user).select_related("borrower", "book")
-
-#
-# class CreateBorrowingView(generics.CreateAPIView):
-#
-#     serializer_class = CreateBorrowingSerializer
-#     permission_classes = (IsAuthenticated, )
-#
-#     def get_serializer_class(self):
-#         if self.request.method == 'POST':
-#             return CreateBorrowingSerializer
-#         return BorrowingSerializer
