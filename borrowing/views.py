e3121029fde9601247a01bf702b916e7fb5322d1
Glieb <gleblitvinenko3007@gmail.com>
2023-08-06 17:43:21 +0300

    BORROWING: implemented filtering feature

--- borrowing/views.py
+++ borrowing/views.py
@@ -7,21 +7,40 @@ from rest_framework.permissions import IsAuthenticated
 
 from book.models import Book
 from borrowing.models import Borrowing
-from borrowing.serializers import BorrowingSerializer, BorrowingDetailSerializer, CreateBorrowingSerializer, \
-    ReturnBorrowSerializer
+from borrowing.serializers import (
+    BorrowingSerializer,
+    BorrowingDetailSerializer,
+    CreateBorrowingSerializer,
+    ReturnBorrowSerializer,
+)
 
 
 class BorrowingListView(generics.ListCreateAPIView):
     queryset = Borrowing.objects.select_related("borrower", "book")
     serializer_class = BorrowingSerializer
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         user = self.request.user
-        return Borrowing.objects.filter(borrower=user).select_related("borrower", "book")
+        queryset_ = Borrowing.objects.select_related("borrower", "book")
+        user_id = self.request.query_params.get("user_id")
+        is_active = self.request.query_params.get("is_active")
+
+        if user_id is not None and user.is_staff:
+            queryset_ = queryset_.filter(borrower=user_id)
+
+        if user.is_staff is False:
+            return queryset_.filter(borrower=user).select_related(
+                "borrower", "book"
+            )
+
+        if is_active is not None:
+            queryset_ = queryset_.filter(actual_return_date__isnull=True)
+
+        return queryset_
 
     def get_serializer_class(self):
-        if self.request.method == 'POST':
+        if self.request.method == "POST":
             return CreateBorrowingSerializer
         return BorrowingSerializer
 
@@ -47,20 +66,24 @@ class BorrowingListView(generics.ListCreateAPIView):
 
 
 class BorrowingDetailView(generics.RetrieveAPIView):
-
     serializer_class = BorrowingDetailSerializer
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated,)
     lookup_field = "id"
 
-    def get_queryset(self):
+    def get_queryset(self, *args, **kwargs):
         user = self.request.user
-        return Borrowing.objects.filter(borrower=user).select_related("borrower", "book")
+        borrowing_id = self.kwargs.get("id")
+        if user.is_staff is False:
+            return Borrowing.objects.filter(borrower=user).select_related(
+                "borrower", "book"
+            )
+        return Borrowing.objects.filter(pk=borrowing_id)
 
 
 class BorrowingReturnView(generics.UpdateAPIView):
     queryset = Borrowing.objects.all()
     serializer_class = ReturnBorrowSerializer
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated,)
     lookup_field = "id"
 
     def perform_update(self, serializer):
