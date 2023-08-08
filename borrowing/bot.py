194ca9a1408a0c8b357c03229ea2275efd6da546
Glieb <gleblitvinenko3007@gmail.com>
2023-08-08 11:21:41 +0300

    BORROWING: implemented send_overdue_and_upcoming_notifications function

--- borrowing/bot.py
+++ borrowing/bot.py
@@ -1,9 +1,13 @@
 import os
+from datetime import timedelta
 
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+from django.utils import timezone
 from dotenv import load_dotenv
+
+from borrowing.models import Borrowing
 
 load_dotenv()
 
@@ -16,6 +20,34 @@ dp = Dispatcher(bot)
 
 async def send_notifications_in_group(notification_message: str):
     await bot.send_message(chat_id=CHAT_ID, text=notification_message)
+
+
+async def send_overdue_and_upcoming_notifications():
+    overdue_borrowings = Borrowing.objects.filter(
+        actual_return_date__isnull=True, expected_return_date__lt=timezone.now()
+    )
+    upcoming_borrowings = Borrowing.objects.filter(
+        actual_return_date__isnull=True,
+        expected_return_date=timezone.now() + timedelta(days=1),
+    )
+
+    daily_message = "Daily borrowings overview\n"
+    if overdue_borrowings or upcoming_borrowings:
+        daily_message += "Overdue borrowings list\n"
+        for borrowing in overdue_borrowings:
+            daily_message += (
+                f"📕 Book: {borrowing.book.title}, 🤠 User: {borrowing.borrower.email}\n"
+            )
+
+        daily_message += "Upcoming borrowings list\n"
+        for borrowing in upcoming_borrowings:
+            daily_message += (
+                f"📕 Book: {borrowing.book.title}, 🤠 User: {borrowing.borrower.email}\n"
+            )
+    else:
+        daily_message += "No overdue and upcoming borrowings"
+
+    await bot.send_message(chat_id=CHAT_ID, text=daily_message)
 
 
 @dp.message_handler(commands=["start"])
