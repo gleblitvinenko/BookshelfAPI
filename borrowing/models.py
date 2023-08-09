43c6c394436351399a9fa3d1df0cdbcd46b77cba
Glieb <gleblitvinenko3007@gmail.com>
2023-08-09 21:30:39 +0300

    BORROWING: implemented Payment model

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,4 @@
+from _decimal import Decimal
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
@@ -7,7 +8,6 @@ from book.models import Book
 
 
 class Borrowing(models.Model):
-
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
@@ -31,3 +31,32 @@ class Borrowing(models.Model):
 
     def __str__(self):
         return f"{self.borrower} borrowed {self.book} on {self.borrow_date}"
+
+
+class Payment(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = "Pending"
+        PAID = "Paid"
+
+    class TypeChoices(models.TextChoices):
+        PAYMENT = "Payment"
+        FINE = "Fine"
+
+    status = models.CharField(max_length=50, choices=StatusChoices.choices)
+    type = models.CharField(max_length=50, choices=TypeChoices.choices)
+    borrowing = models.ForeignKey(
+        Borrowing, related_name="borrowing", on_delete=models.CASCADE
+    )
+    session_url = models.URLField()
+    session_id = models.CharField(max_length=255)
+    money_to_pay = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def save(self, *args, **kwargs):
+        if self.borrowing.actual_return_date and self.borrowing:
+            days_borrowed = (
+                self.borrowing.actual_return_date - self.borrowing.borrow_date
+            ).days
+            money_to_pay = Decimal(days_borrowed) * self.borrowing.book.daily_fee
+            self.money_to_pay = money_to_pay
+
+        super().save(*args, **kwargs)
