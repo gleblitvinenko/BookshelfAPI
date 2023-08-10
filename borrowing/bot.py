8cdf39ffaad6e15c5b6da2118b28b6e7c45c0b8b
Glieb <gleblitvinenko3007@gmail.com>
2023-08-10 14:58:34 +0300

    LIBRARY-SERVICE: switched django-q to celery

--- borrowing/bot.py
+++ borrowing/bot.py
@@ -21,39 +21,5 @@ dp = Dispatcher(bot)
 async def send_notifications_in_group(notification_message: str):
     await bot.send_message(chat_id=CHAT_ID, text=notification_message)
 
-
-async def send_overdue_and_upcoming_notifications():
-    overdue_borrowings = Borrowing.objects.filter(
-        actual_return_date__isnull=True, expected_return_date__lt=timezone.now()
-    )
-    upcoming_borrowings = Borrowing.objects.filter(
-        actual_return_date__isnull=True,
-        expected_return_date=timezone.now() + timedelta(days=1),
-    )
-
-    daily_message = "Daily borrowings overview\n"
-    if overdue_borrowings or upcoming_borrowings:
-        daily_message += "Overdue borrowings list\n"
-        for borrowing in overdue_borrowings:
-            daily_message += (
-                f"📕 Book: {borrowing.book.title}, 🤠 User: {borrowing.borrower.email}\n"
-            )
-
-        daily_message += "Upcoming borrowings list\n"
-        for borrowing in upcoming_borrowings:
-            daily_message += (
-                f"📕 Book: {borrowing.book.title}, 🤠 User: {borrowing.borrower.email}\n"
-            )
-    else:
-        daily_message += "No overdue and upcoming borrowings"
-
-    await bot.send_message(chat_id=CHAT_ID, text=daily_message)
-
-
-@dp.message_handler(commands=["start"])
-async def send_welcome(message: types.Message):
-    await message.reply("Hi, it is start message!")
-
-
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=False)
--- borrowing/tasks.py
+++ borrowing/tasks.py
@@ -1,14 +1,32 @@
+from django.db.models import Q
 from django.utils import timezone
-from django_q.models import Schedule
 
-# from bot import send_overdue_and_upcoming_notifications
+from borrowing.models import Borrowing
+from celery import shared_task
+from bot import send_notifications_in_group
 
 
-def create_daily_task():
-    Schedule.objects.create(
-        func="bot.send_overdue_and_upcoming_notifications",
-        schedule_type=Schedule.MINUTES,
-        minutes=1,
-        repeats=5,
-        next_run=timezone.now(),
+@shared_task
+def check_borrowing_overdue():
+    """The function filter all borrowings,
+    which are overdue
+    and send a notification to the telegram chat
+    about each overdue separately with detailed information
+    """
+    today = timezone.now()
+    overdue_borrowing = Borrowing.objects.filter(
+        Q(expected_return_date__lte=today) & Q(actual_return_date__isnull=True)
     )
+
+    message = "No borrowings overdue today!"
+
+    for borrowing in overdue_borrowing:
+        message = (
+            f"📕 The borrowing of the book "
+            f"{borrowing.book.title}\n"
+            f"🤠 User: {borrowing.user.first_name}"
+            f"is overdue.\n"
+            f"📅 The expected return date was\n"
+            f"{borrowing.expected_return_date}."
+        )
+    send_notifications_in_group(message)
